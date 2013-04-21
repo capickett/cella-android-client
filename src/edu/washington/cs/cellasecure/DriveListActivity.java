04bfc2a34dd130b52843744d902111fefe5f2d13
Cameron Pickett <pickett.cj@gmail.com>
2013-04-21 00:31:15 -0700

    Allow filtering devices
    Filter between available and out of range devices

--- src/edu/washington/cs/cellasecure/DriveListActivity.java
+++ src/edu/washington/cs/cellasecure/DriveListActivity.java
@@ -1,10 +1,13 @@
 package edu.washington.cs.cellasecure;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 
 /**
  * An activity representing a list of Drives. This activity has different
@@ -22,71 +25,85 @@ import android.view.MenuInflater;
  * {@link DriveListFragment.Callbacks} interface to listen for item selections.
  */
 public class DriveListActivity extends FragmentActivity implements
-        DriveListFragment.Callbacks {
+		DriveListFragment.Callbacks,
+		ActionBar.OnNavigationListener {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
+	/**
+	 * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+	 * device.
+	 */
+	private boolean mTwoPane;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_drive_list);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_drive_list);
 
-        if (findViewById(R.id.drive_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-large and
-            // res/values-sw600dp). If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
+		SpinnerAdapter mDropDownAdapter = ArrayAdapter.createFromResource(this,
+				R.array.action_list,
+				android.R.layout.simple_spinner_dropdown_item);
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayShowTitleEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		actionBar.setListNavigationCallbacks(mDropDownAdapter, this);
 
-            // In two-pane mode, list items should be given the
-            // 'activated' state when touched.
-            ((DriveListFragment) getSupportFragmentManager().findFragmentById(
-                    R.id.drive_list)).setActivateOnItemClick(true);
-        }
+		if (findViewById(R.id.drive_detail_container) != null) {
+			// The detail container view will be present only in the
+			// large-screen layouts (res/values-large and
+			// res/values-sw600dp). If this view is present, then the
+			// activity should be in two-pane mode.
+			mTwoPane = true;
 
-        // TODO: If exposing deep links into your app, handle intents here.
-    }
-    
+			// In two-pane mode, list items should be given the
+			// 'activated' state when touched.
+			((DriveListFragment) getSupportFragmentManager().findFragmentById(
+					R.id.drive_list)).setActivateOnItemClick(true);
+		}
 
-    /* (non-Javadoc)
-     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflator = getMenuInflater();
-        inflator.inflate(R.menu.device_list, menu);
-        inflator.inflate(R.menu.settings, menu);
-        return true;
-    }
+		// TODO: If exposing deep links into your app, handle intents here.
+	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflator = getMenuInflater();
+		inflator.inflate(R.menu.device_list, menu);
+		inflator.inflate(R.menu.settings, menu);
+		return true;
+	}
 
-    /**
-     * Callback method from {@link DriveListFragment.Callbacks} indicating that
-     * the item with the given ID was selected.
-     */
-    @Override
-    public void onItemSelected(String id) {
-        if (mTwoPane) {
-            // In two-pane mode, show the detail view in this activity by
-            // adding or replacing the detail fragment using a
-            // fragment transaction.
-            Bundle arguments = new Bundle();
-            arguments.putString(DriveDetailFragment.ARG_ITEM_ID, id);
-            DriveDetailFragment fragment = new DriveDetailFragment();
-            fragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.drive_detail_container, fragment).commit();
+	/**
+	 * Callback method from {@link DriveListFragment.Callbacks} indicating that
+	 * the item with the given ID was selected.
+	 */
+	@Override
+	public void onItemSelected(String id) {
+		if (mTwoPane) {
+			// In two-pane mode, show the detail view in this activity by
+			// adding or replacing the detail fragment using a
+			// fragment transaction.
+			Bundle arguments = new Bundle();
+			arguments.putString(DriveDetailFragment.ARG_ITEM_ID, id);
+			DriveDetailFragment fragment = new DriveDetailFragment();
+			fragment.setArguments(arguments);
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.drive_detail_container, fragment).commit();
 
-        } else {
-            // In single-pane mode, simply start the detail activity
-            // for the selected item ID.
-            Intent detailIntent = new Intent(this, DriveDetailActivity.class);
-            detailIntent.putExtra(DriveDetailFragment.ARG_ITEM_ID, id);
-            startActivity(detailIntent);
-        }
-    }
+		} else {
+			// In single-pane mode, simply start the detail activity
+			// for the selected item ID.
+			Intent detailIntent = new Intent(this, DriveDetailActivity.class);
+			detailIntent.putExtra(DriveDetailFragment.ARG_ITEM_ID, id);
+			startActivity(detailIntent);
+		}
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		return false;
+	}
 }

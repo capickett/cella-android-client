e0da4a5addc487c5ea075ab9c329de297824739d
Cameron Pickett <pickett.cj@gmail.com>
2013-05-24 09:22:38 -0700

    Licensing

--- src/edu/washington/cs/cellasecure/fragments/DriveUnlockFragment.java
+++ src/edu/washington/cs/cellasecure/fragments/DriveUnlockFragment.java
@@ -1,17 +1,30 @@
+/*
+ * Copyright 2013 CellaSecure
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package edu.washington.cs.cellasecure.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 import edu.washington.cs.cellasecure.Drive;
-import edu.washington.cs.cellasecure.DriveConnectActivity;
 import edu.washington.cs.cellasecure.R;
 import edu.washington.cs.cellasecure.bluetooth.BluetoothUtility;
 import edu.washington.cs.cellasecure.bluetooth.Connection;
@@ -78,7 +91,9 @@ public class DriveUnlockFragment extends Fragment implements View.OnClickListene
     }
 
     @Override
-    public void onConnected(BluetoothDevice device, Connection connection) { mConnection = connection; }
+    public void onConnected(BluetoothDevice device, Connection connection) {
+        mConnection = connection;
+    }
 
     @Override
     public void onDiscovery(List<BluetoothDevice> bluetoothDevices) { /* Do nothing */ }
